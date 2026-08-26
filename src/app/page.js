d4e8bb56d7c2f5ffd25cb7c1a1d7513d6b91961f
cauaprs92 <cauaperes92@gmail.com>
2026-08-26 08:18:59 -0300

    commit "alteração na foto do popup 03 x honey"

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 const menuItems = [
   ["Monster Burger", "Duplo smash, cheddar, bacon e molho Stalker.", "lanche1_stalkerburguer.png"],
   ["Piscina Cheddar", "Crocante por fora, cremoso no centro e impossível de ignorar.", "piscinacheddar-StalkerBurguer.png"],
-  ["X Honey", "Blend artesanal, queijo coalho e o toque doce do mel.", "lanche3_stalkerburguer.png"],
+  ["X Honey", "Blend artesanal, queijo coalho e o toque doce do mel.", "lache-queijudo-StalkerBurguer.png"],
   ["Costela Stalker", "Costela desfiada, queijo derretido e pão macio.", "lanche4_stalkerburguer.png"],
 ];
 
